bdd0e54233e9777f10ff54fd19055a6867c7e90f
Marek Wannepain <marekwannepain@gmail.com>
2025-02-14 16:53:43 +0100

    Add validation and error handling for cancel_subscription endpoint

--- app.py
+++ app.py
@@ -124,7 +124,22 @@ def create_subscription():
 @cross_origin()
 def cancel_subscription():
     data = request.json
-    subscription_id = data["subscriptionId"]
+    if not data or not data["uid"]:
+        return jsonify({"error": "no uid specified"})
+    user_id = data["uid"]
+
+    # Get user from Firestore
+    db = firestore.client()
+    user_ref = db.collection("users").document(user_id).get()
+    if not user_ref.exists:
+        return jsonify({"error": "User not found"}), 404
+
+    user_data = user_ref.to_dict()
+    print(user_data)
+    if not "subscriptionId" in user_data:
+        return jsonify({"susbcription_type": "free", "data": None})
+
+    subscription_id = user_data["subscriptionId"]
 
     try:
         stripe.Subscription.delete(subscription_id)
